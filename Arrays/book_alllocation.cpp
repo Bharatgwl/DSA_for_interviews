666982162a56cf2d04233465b094f21b9ab9cdb9
Bharat Gusaiwal <bharatgusaiwal731@gmail.com>
2024-10-16 23:40:07 +0530

    new question

---
+++ Arrays/book_alllocation.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool ispossible(vector<int> arr, int size, int m, int mid)
+{
+    int stdcount = 1;
+    int pagesum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (pagesum + arr[i] > mid)
+        {
+            stdcount++;
+            pagesum = arr[i];
+            if (stdcount > m || arr[i] > mid)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            pagesum += arr[i];
+        }
+    }
+    return true;
+}
+int allocatebooks(vector<int> arr, int size, int m)
+{
+    int s = 0;
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    int e = sum;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (ispossible(arr, size, m, mid))
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr = {10, 20, 30, 40};
+    int n = arr.size();
+    int m = 2;
+    int answers = allocatebooks(arr, n, m);
+    cout << answers << endl;
+    return 0;
+}
