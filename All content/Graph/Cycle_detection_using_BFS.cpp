e7a9e38716dde47f0c396754acf66f3a6f78d59b
Bharat Gusaiwal <bharatgusaiwal731@gmail.com>
2025-01-04 17:42:58 +0530

    GRAPH problems

---
+++ All content/Graph/Cycle_detection_using_BFS.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <unordered_map>
+#include <set> //to store in a sorted order
+#include <vector>
+#include <queue>
+using namespace std;
+
+class graph
+{
+public:
+    unordered_map<int, set<int>> adj;
+    void addEdge(int x, int y, int directed)
+    {
+        adj[x].insert(y);
+        if (directed == 0)
+        {
+            adj[y].insert(x);
+        }
+    }
+    void printAdjset()
+    {
+        for (auto i : adj)
+        {
+            cout << i.first << "->";
+            for (auto j : i.second)
+            {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+};
+
+bool traverse(int node, unordered_map<int, set<int>> &adj, unordered_map<int, bool> &visited)
+{
+    queue<int> q;
+
+    q.push(node);
+    visited[node] = true;
+    unordered_map<int, int> parent;
+    parent[node] = -1;
+    while (!q.empty())
+    {
+        int frontnode = q.front();
+        q.pop();
+        for (auto neighbour : adj[frontnode])
+        {
+            if (!visited[neighbour])
+            {
+                q.push(neighbour);
+                visited[neighbour] = true;
+                parent[neighbour] = frontnode;
+            }
+            else if (neighbour != parent[frontnode])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool isCyclic(int n, unordered_map<int, set<int>> &adj)
+{
+    unordered_map<int, bool> visited;
+    bool ans;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            ans = traverse(i, adj, visited);
+        }
+    }
+    return ans;
+}
+int main()
+{
+    graph g1;
+    int n;
+    cout << "enter the number of nodes" << endl;
+    cin >> n;
+    int m;
+    cout << "enter the number of edges" << endl;
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        g1.addEdge(x, y, 0);
+    }
+
+    vector<int> ans;
+
+    if (isCyclic(n, g1.adj))
+    {
+        cout << "graph contains cycle" << endl;
+    }
+    else
+    {
+        cout << "graph does not contain cycle" << endl;
+    }
+    return 0;
+}
