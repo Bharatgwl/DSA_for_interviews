49ce56d37c4c39d03300a58ca9d84a6ee6424f5a
Bharat Gusaiwal <bharatgusaiwal731@gmail.com>
2025-04-21 17:27:38 +0530

    License updated

--- All content/file_handling/1st.cpp
+++ All content/file_handling/1st.cpp
@@ -1,16 +1,37 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-int main(){
-    fstream file("input.txt");
-  if (file.is_open()){
-    string line;
-    while(getline(file,line)){
-       cout<<line<<endl;
+bool ispalindrome(string str)
+{
+  int n = str.length();
+  for (int i = 0; i < n / 2; i++)
+  {
+    if (str[i] != str[n - i - 1])
+    {
+      return false;
     }
-    file.close();
   }
-
-    
-    
+  return true;
+}
+bool iskpalindromic(int num)
+{
+  
+}
+int main()
+{
+  int n = 5;
+  int count = 0;
+  for (int i = 100; i < 999; i++)
+  {
+    string str = to_string(i);
+    if (ispalindrome(str))
+    {
+      if (i % 5 == 0)
+      {
+        count++;
+      }
+    }
+  }
+  cout << count << endl;
+  return 0;
 }
